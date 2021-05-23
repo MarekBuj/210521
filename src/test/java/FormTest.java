8e51c1535e99bb3f40eba85abac8ddcf3b64d87d
Marek <b3.marek@gmail.com>
2021-05-23 14:20:54 +0200

    Annotations Test

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -13,6 +13,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.util.List;
 import java.util.Random;
 
@@ -33,7 +34,7 @@ public class FormTest {
         wait = new WebDriverWait(driver, 10);
     }
 
-    @Test(invocationCount = 10)
+    @Test(invocationCount = 1)
     public void shouldFillOutTheForm() {
         driver.get("https://seleniumui.moderntester.pl/form.php");
 
@@ -65,9 +66,18 @@ public class FormTest {
 
         Select continentSelect = new Select(driver.findElement(By.id("selectContinents")));
         List<WebElement> continentSelectOptions = continentSelect.getOptions();
-        continentSelectOptions.remove(0);
+        continentSelectOptions.remove(0); //usuwamy choose
         WebElement randomContinent = getRandomElement(continentSelectOptions);
         continentSelect.selectByVisibleText(randomContinent.getText());
+
+        //continentSelect.selectByValue("europe");
+        Select seleniumCommands = new Select(driver.findElement(By.id("selectSeleniumCommands")));
+        List<WebElement> seleniumCommandsOptions = seleniumCommands.getOptions();
+        getRandomElement(seleniumCommandsOptions).click();
+
+        WebElement fileInput = driver.findElement(By.id("chooseFile"));
+        File file = new File("src/main/resources/emptyFile.txt");
+        fileInput.sendKeys(file.getAbsolutePath());
 
         WebElement additionalInfo = driver.findElement(By.cssSelector("#additionalInformations"));
         additionalInfo.sendKeys("TEST");
