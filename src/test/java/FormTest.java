fd22506c8d47bd9c31f1fc28dadc4b7f1bc38de7
Marek <b3.marek@gmail.com>
2021-05-23 11:22:43 +0200

    Annotations Test

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FormTest {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+@BeforeMethod
+    public void setup() {
+    //System.setProperty("webdriver.chrome.driver", "ścieżka do chromedriver");
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    //options.addArguments("start-maximized");
+    options.addArguments("disable-extensions");
+    driver = new ChromeDriver(options);
+    driver.manage().window().setPosition(new Point(3440,0)); //NIE KOPIOWAC
+    wait = new WebDriverWait(driver, 10);
+}
+
+@Test(invocationCount = 1)
+public void shouldFillOutTheForm(){
+    driver.get("https://seleniumui.moderntester.pl/form.php");
+
+}
+
+@AfterMethod
+    public void tearDown() {driver.quit();}
+
+}
