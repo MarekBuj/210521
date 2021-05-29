5a389ed6f3991a0a7d442387aa8943d36581efb1
Marek <b3.marek@gmail.com>
2021-05-29 09:50:52 +0200

    Annotations Test

--- src/test/java/Day5/IframeTest.java
+++ src/test/java/Day5/IframeTest.java
@@ -1,6 +1,8 @@
 package Day5;
 
 import Day4.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class IframeTest extends TestBase {
@@ -11,6 +13,21 @@ public class IframeTest extends TestBase {
 
         // 1st frame
         driver.switchTo().frame("iframe1");
+        driver.findElement(By.id("inputFirstName3")).sendKeys("Jan");
+        driver.findElement(By.id("inputSurname3")).sendKeys("Wiadro");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        // 2nd frame
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame("iframe2");
+
+        driver.findElement(By.id("inputLogin")).sendKeys("Tomasz");
+        driver.findElement(By.id("inputPassword")).sendKeys("1234");
+        new Select(driver.findElement(By.id("inlineFormCustomSelectPref"))).selectByIndex(1);
+        driver.findElement(By.id("gridRadios4")).click();
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+
 
         driver.switchTo().defaultContent();
     }
