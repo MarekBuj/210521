d3a4de51c1c4266a81bc0ed80c3754bbca5cf8c8
Marek <b3.marek@gmail.com>
2021-05-29 14:51:48 +0200

    Annotations Test

--- src/main/java/Day5/FormPO.java
+++ src/main/java/Day5/FormPO.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.List;
 import java.util.Random;
 
@@ -22,6 +24,93 @@ public class FormPO {
     @FindBy(css = "#inputLastName3")
     private WebElement inputLastName;
 
+    @FindBy(css = "#inputEmail3")
+    private WebElement inputEmail;
+
+    @FindBy(name = "gridRadiosExperience")
+    private List<WebElement> yearsOfExperience;
+
+    @FindBy(css = "input[value='male']")
+    private WebElement maleRadioButton;
+
+    @FindBy(id = "gridCheckManulTester")
+    private WebElement manualTesterGrid;
+
+    @FindBy(id = "selectContinents")
+    private WebElement selectContinents;
+
+    @FindBy(id = "selectSeleniumCommands")
+    private WebElement commands;
+
+    @FindBy(id = "chooseFile")
+    private WebElement fileInput;
+
+    @FindBy(id = "additionalInformations")
+    private WebElement additionalInformationTextBox;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement submitButton;
+
+    @FindBy(id = "validator-message")
+    private WebElement validatorMessage;
+
+    @FindBy(id = "inputAge3")
+    private WebElement ageTextBox;
+
+    public void selectRandomExperience() {
+        getRandomElement(yearsOfExperience).click();
+    }
+
+    public void fillAge(int age) {
+        ageTextBox.sendKeys(Integer.toString(age));
+    }
+
+    public String getValidatorMessageText() {
+        return validatorMessage.getText();
+    }
+
+
+    public void submit() {
+        submitButton.click();
+    }
+
+
+    public void fillAdditionalInformation(String text) {
+        additionalInformationTextBox.sendKeys(text);
+    }
+
+
+    public void fillFileInput() {
+        File file = new File("src/main/resources/emptyFile.txt");
+        fileInput.sendKeys(file.getAbsolutePath());
+    }
+
+    public void selectRandomCommand() {
+        Select commandSelect = new Select(commands);
+        List<WebElement> commandsOptions = commandSelect.getOptions();
+        getRandomElement(commandsOptions).click();
+    }
+
+
+    public void selectRandomContinent() {
+        Select continents = new Select(selectContinents);
+        List<WebElement> continentsOptions = continents.getOptions();
+        continentsOptions.remove(0);
+        WebElement randomOption = getRandomElement(continentsOptions);
+        continents.selectByVisibleText(randomOption.getText());
+    }
+
+    public void selectManualTester() {
+        manualTesterGrid.click();
+    }
+
+    public void selectMale() {
+        maleRadioButton.click();
+    }
+
+    public void fillEmail(String email) {
+        inputEmail.sendKeys(email);
+    }
 
     public void fillName(String firstName, String lastName) {
         inputFirstName.sendKeys(firstName);
--- src/test/java/Day5/FormPoTest.java
+++ src/test/java/Day5/FormPoTest.java
@@ -1,6 +1,7 @@
 package Day5;
 
 import Day4.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FormPoTest extends TestBase {
@@ -11,6 +12,19 @@ public class FormPoTest extends TestBase {
         formPO.ownUrl = "https://seleniumui.moderntester.pl/form.php";
         formPO.openMe();
         formPO.fillName("Jan","Kowalski");
+        formPO.fillName("Bogdan", "Wiadro");
+        formPO.fillEmail("test@test.com");
+        formPO.selectMale();
+        formPO.fillAge(45);
+        formPO.selectRandomExperience();
+        formPO.selectManualTester();
+        formPO.selectRandomContinent();
+        formPO.selectRandomCommand();
+        formPO.fillFileInput();
+        formPO.fillAdditionalInformation("Tralalala");
+        formPO.submit();
+
+        Assert.assertEquals(formPO.getValidatorMessageText(), "Form send with success");
     }
 
 }
